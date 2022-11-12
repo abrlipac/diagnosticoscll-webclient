b5afcda17041f0c193c47db558d88fd41f1cfe11
Abraham Lipa Calabilla <abrlipac@upt.pe>
2022-11-12 02:06:51 -0500

    Corrige pnpm error

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -1,6 +1,6 @@
 export { matchers } from './client-matchers.js';
 
-			export const nodes = [() => import('./nodes/0'),
+export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
@@ -12,9 +12,9 @@ export { matchers } from './client-matchers.js';
 	() => import('./nodes/9'),
 	() => import('./nodes/10')];
 
-			export const server_loads = [];
+export const server_loads = [];
 
-			export const dictionary = {
+export const dictionary = {
 	"/": [2],
 	"/diagnosticos/editar": [3],
 	"/diagnosticos/nuevo": [4],
@@ -26,6 +26,6 @@ export { matchers } from './client-matchers.js';
 	"/signup": [10]
 };
 
-			export const hooks = {
-				handleError: (({ error }) => { console.error(error) }),
-			};
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+};
